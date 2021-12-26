0b79e94f04dd552ef4ba4d4acd117e5aeca79696
HabibIbrahimov <habibyi@code.edu.az>
2021-12-26 23:18:29 +0400

    updated js

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
-async function getNamazTimes() {
-    const res = await axios.get('https://api.aladhan.com/v1/calendar?latitude=40.409264&longitude=49.867092&method=2&month=4&year=2017');
-    let lorem = (res.data.data);
-    let dataa=`04-04-2017`
-    for (const item of lorem) {
-        let jsonDate = (item.date.gregorian.date);
-        if(dataa===`${jsonDate}`){
-            console.log("okey")
-            break;
-        }
-        console.log(jsonDate);
-    }
-}
-
-document.getElementById("sub").click=getNamazTimes()
+let date = document.getElementById("date");
+let btn = document.getElementById("btn");
+let div = document.getElementById("div");
+1995-12-12
+btn.addEventListener("click", function () {
+  event.preventDefault();
+  let arr = date.value.split("-");
+  let long=50;
+  let url = `http://api.aladhan.com/v1/calendar?latitude=49&longitude=40&method=2&month=${arr[1]}&year=${arr[0]}`;
+  fetch(url)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      return data.data;
+    })
+    .then(function (data) {
+      console.log(data);
+      let dateValue = date.value.split("-").reverse().join("-");
+      let object = data.filter((d) => d.date.gregorian.date === dateValue)[0];
+      div.innerHTML = `
+    <p><h1>${object.date.readable}</h1></p>
+     <p><h3>Fajr:</h3><span>${data[0].timings.Fajr}</span</p>
+     <p><h3>Sunrise:</h3>${data[0].timings.Sunrise}</span</p>
+     <p><h3>Dhuhr:</h3>${data[0].timings.Dhuhr}</span</p>
+     <p><h3>Asr:</h3>${data[0].timings.Asr}</span</p>
+     <p><h3>Sunset:</h3>${data[0].timings.Sunset}</span</p>
+     <p><h3>Maghrib:</h3>${data[0].timings.Maghrib}</span</p>
+     <p><h3>Isha:</h3>${data[0].timings.Isha}</span</p>
+     <p><h3>Midnight:</h3>${data[0].timings.Midnight}</span</p>    
+    `;
+      date.value = "";
+    });
+});
